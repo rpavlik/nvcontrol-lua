ea05bb84f559c52b877d3b4b2da1d9d790f934f1
Ryan Pavlik <rpavlik@iastate.edu>
2011-07-01 11:12:44 -0500

    update the challenger framelock script to use the new module

--- challenger.lua
+++ challenger.lua
@@ -1,9 +1,9 @@
 #!/usr/bin/env lua
-require("framelock")
+require("nvcontrol.framelock")
 --[[ Configuration Section ]]--
 
 --[[
-challenger.vrac:~/src/framelocker> nvidia-settings -q screens -q gpus -q framelocks -q vcs -q gvis -q fans
+challenger.vrac:~/src/framelocker> nvidia-settings -q screens -q gpus -q framelocks -q vcs
 
 4 X Screens on challenger.vrac.iastate.edu:0
 
@@ -28,14 +28,8 @@ challenger.vrac:~/src/framelocker> nvidia-settings -q screens -q gpus -q framelo
 1 VCS on challenger.vrac.iastate.edu:0
 
     [0] challenger.vrac.iastate.edu:0[vcs:0] (NVIDIA QuadroPlex 7000)
-
-2 Fans on challenger.vrac.iastate.edu:0
-
-    [0] challenger.vrac.iastate.edu:0[fan:0] (Fan 0)
-
-    [1] challenger.vrac.iastate.edu:0[fan:1] (Fan 1)
-
 ]]
+
 host = os.getenv("HOST")
 if host == "challenger.vrac.iastate.edu" then
 	print("OK, running on Challenger")
@@ -47,58 +41,27 @@ else
 	print("You are brave - not running on either machine!")
 	challenger, columbia = "challenger.vrac.iastate.edu", "columbia.vrac.iastate.edu"
 end
-screens = {
-	Screen(challenger .. ":0.0"){
-		gpu = challenger .. ":0[gpu:0]",
-		framelock = challenger .. ":0[framelock:0]"
-	},
-	Screen(challenger .. ":0.1"){
-		gpu = challenger .. ":0[gpu:0]",
-		framelock = challenger .. ":0[framelock:0]",
-		master = true
-	},
-	Screen(challenger .. ":0.2"){
-		gpu = challenger .. ":0[gpu:1]",
-		framelock = challenger .. ":0[framelock:0]"
-	},
-	Screen(challenger .. ":0.3"){
-		gpu = challenger .. ":0[gpu:1]",
-		framelock = challenger .. ":0[framelock:0]"
-	},
-	--[[
-	Screen(columbia .. ":0.0"){
-		gpu = columbia .. ":0[gpu:0]",
-		--display = primaryDVI,
-		framelock = columbia .. ":0[framelock:0]"
-	},
-	Screen(columbia .. ":0.1"){
-		gpu = columbia .. ":0[gpu:0]",
-		--display = primaryDVI,
-		framelock = columbia .. ":0[framelock:0]"
-	},
-	Screen(columbia .. ":0.2"){
-		gpu = columbia .. ":0[gpu:1]",
-		--display = primaryDVI,
-		framelock = columbia .. ":0[framelock:0]"
-	},
-	Screen(columbia .. ":0.3"){
-		gpu = columbia .. ":0[gpu:1]",
-		--display = primaryDVI,
-		framelock = columbia .. ":0[framelock:0]"
-	},]]
+
+masters = {
+	nvcontrol.XScreen(challenger .. ":0.0"),
+}
+nonmasters = {
+	nvcontrol.XScreen(challenger .. ":0.1"),
+	nvcontrol.XScreen(challenger .. ":0.2"),
+	nvcontrol.XScreen(challenger .. ":0.3"),
+	--nvcontrol.XScreen(columbia .. ":0.0"),
 }
 
-options = {
-	on = enableFramelock;
-	off = disableFramelock;
+commands = {
+	on = nvcontrol.framelock.enable;
+	off = nvcontrol.framelock.disable;
 }
 
-if #arg < 1 then
-	-- call main function
-	enableFramelock()
-elseif options[arg[1]] ~= nil then
-	options[arg[1]]()
-else
-	error("Unrecognized option " .. arg[1])
+command = "on"
+if arg[1] ~= nil and commands[arg[1]] ~= nil then
+	command = arg[1]
 end
+
+-- Perform the requested command
+commands[command](masters, nonmasters)
 
