41ad5b75fdf1f02029b0a8537e39a68a38f59b36
Ryan Pavlik <rpavlik@iastate.edu>
2011-07-01 10:19:55 -0500

    improve comments

--- nvcontrol.lua
+++ nvcontrol.lua
@@ -1,3 +1,28 @@
+--[[
+nvcontrol.lua
+
+A dynamic binding to the nvcontrol capabilities accessed through the
+nvidia-settings command line application.
+
+nvcontrol.XScreen( <screen identifier> ) - returns an object representing
+an X screen.  Attributes can be accessed with . for getting and setting,
+where permitted.
+
+nvcontrol.verbose - set to true to see all command lines executed along with
+additional debug info.
+
+Example:
+
+$ lua -l nvcontrol
+Lua 5.1.4  Copyright (C) 1994-2008 Lua.org, PUC-Rio
+> s = nvcontrol.XScreen(":0.0")
+> print(tostring(s))
+nvcontrol.XScreen(":0.0")
+> print(s.RefreshRate)
+
+]]
+--[[ Utility Functions ]]
+
 local function do_command(cmd)
 	print("Running: " .. cmd)
 	os.execute(cmd)
@@ -17,6 +42,11 @@ local function trim(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
+
+--[[ Data ]]
+nvcontrol = {}
+
+--[[ Metatable Methods ]]
 local function setAttribute(tgt, attr, value)
 	if value == true then
 		value = 1
@@ -58,6 +88,7 @@ local function screenToString(screen)
 	return ([[nvcontrol.XScreen("%s")]]):format(screen.id)
 end
 
+--[[ Object Creation Methods ]]
 local XScreenMT = {__index = getAttribute, __newindex = setAttribute, __tostring = screenToString }
 local function createXScreen(name)
 	local s = { id = name, ctrldisplay = name }
@@ -65,8 +96,10 @@ local function createXScreen(name)
 	return s
 end
 
+--[[ Initialization ]]
 nvcontrol = {
 	XScreen = createXScreen
 }
+nvcontrol.XScreen = createXScreen
 
 return nvcontrol
